c06bdf4410cb25df1b9e37f9c0c3564dd8995ce0
rebeccarose123 <rebecca.rose1836@yahoo.com>
2018-07-24 15:20:47 -0400

    finish part 2

--- part-2/dog_show.rb
+++ part-2/dog_show.rb
@@ -7,4 +7,47 @@ class DogShow
     @calculator = args.fetch(:calculator) { PointsCalculator.new }
   end
 
-end
+  def rankings
+    rounds = @dogs.length - 1
+    sort_dogs = @dogs
+
+    loop do 
+      swapped = false
+
+      (rounds).times do |i|
+        if @calculator.points_for_dog(sort_dogs[i]) < @calculator.points_for_dog(sort_dogs[i + 1])
+           sort_dogs[i], sort_dogs[i + 1] = sort_dogs[i + 1], sort_dogs[i]
+          
+          swapped = true
+        end 
+      end 
+      break if not swapped 
+    end 
+    sort_dogs
+  end 
+
+  def best_dogs
+    best_half = @dogs.length * (0.5)
+    return rankings[0...best_half]
+  end 
+
+  def ranking_for(pup)
+    dog_name = pup
+    sorted_dogs = rankings
+    loops = 1
+
+     sorted_dogs.each do |i|
+       if dog_name == i.name
+        lreturn loops
+       else 
+        loops += 1
+       end 
+      end 
+   end 
+
+end 
+
+
+
+
+
