c4471e42d7080345b7c8b18ec88b80457c56fdfa
rebeccarose123 <rebecca.rose1836@yahoo.com>
2018-07-29 14:18:44 -0400

    first part of 4

--- part-3/media_kiosk.rb
+++ part-3/media_kiosk.rb
@@ -9,19 +9,4 @@ class MediaKiosk < I_status
     @items = args[:items]
   end
 
-  # def add_item(item)
-  #   items << item
-  # end
-
-  # def remove_item(item)
-  #   items.delete(item)
-  # end
-
-  # def available_items
-  #   items.select { |item| item.available? && !item.damaged? }
-  # end
-
-  # def unavailable_items
-  #   items - available_items
-  # end
 end
